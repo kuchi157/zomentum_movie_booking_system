489acb06fbe79d351aa77963afd7818282fedfe8
Gyanendra Priyadarshi <157kuchi@gmail.com>
2020-08-30 19:56:18 +0530

    endpoint to update a ticket timing

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ app.post("/bookTicket", async (req, res) => {
   timeDiff = moment().diff(utcTime, "seconds");
 
   if (timeDiff > 0) {
-    return res.send("Ticket cannot be booked. Movie already started or over ");
+    return res
+      .status(406)
+      .send("Ticket cannot be booked. Movie already started or over ");
   }
 
   const oldTicket = await Ticket.find({
@@ -24,15 +26,48 @@ app.post("/bookTicket", async (req, res) => {
   });
 
   if (oldTicket.length >= 20) {
-    return res.send(
-      "Sorry no ticket is available. All tickets are already being booked."
-    );
+    return res
+      .status(404)
+      .send(
+        "Sorry no ticket is available. All tickets are already being booked."
+      );
   }
   const ticket = new Ticket(req.body);
   try {
     await ticket.save();
 
     res.status(201).send({ ticket });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.patch("/updateTiming/:id", async (req, res) => {
+  const time = moment(req.body.showtime, "DD-MM-YYYY hh:mm");
+  const utcTime = time.toISOString();
+
+  timeDiff = moment().diff(utcTime, "seconds");
+
+  if (timeDiff > 0) {
+    return res
+      .status(406)
+      .send("Ticket time cannot be updated. Movie already started or over ");
+  }
+  try {
+    ticket = await Ticket.findOneAndUpdate(
+      { _id: req.params.id },
+      { showtime: req.body.showtime, UTCtiming: utcTime },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (ticket == null) {
+      return res.status(404).send("Invalid Ticket Id!");
+    }
+
+    res.send(ticket);
   } catch (e) {
     res.status(400).send(e);
   }
